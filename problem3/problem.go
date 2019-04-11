bee902184552236166e86d07517b509e31dad1cd
Krzysztof Niepokojczycki <krzysztof.niepokojczycki@gmail.com>
2019-04-11 12:57:06 +0200

    add problem 3

---
+++ problem3/problem.go
@@ -0,0 +1,34 @@
+package problem3
+
+import (
+	"fmt"
+	"math"
+)
+
+//The prime factors of 13195 are 5, 7, 13 and 29.
+//What is the largest prime factor of the number 600851475143 ?
+func largestPrimeFactor(value int) int {
+	maxPrime := -1
+
+	for value%2 == 0 {
+		value >>= 1
+	}
+
+	for i := 3; float64(i) <= math.Sqrt(float64(value)); i += 2 {
+		for value%i == 0 {
+			maxPrime = i
+			value = value/i
+		}
+ 	}
+
+	if value > 2 {
+		maxPrime = value
+	}
+
+	return maxPrime
+}
+
+
+func Result() {
+	fmt.Printf("Result for 600851475143 is %d\n", largestPrimeFactor(600851475143))
+}
---
+++ problem3/problem_test.go
@@ -0,0 +1,33 @@
+package problem3
+
+import "testing"
+
+func Test_largestPrimeFactor(t *testing.T) {
+	type args struct {
+		i int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			"test case #1",
+			args{13195},
+			29,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := largestPrimeFactor(tt.args.i); got != tt.want {
+				t.Errorf("largestPrimeFactor() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+
+func Test_Result(t *testing.T) {
+	Result()
+}
+
