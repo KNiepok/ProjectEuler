d4e49c112c9188f66ca29ee7bf4389e8f654fc6f
Krzysztof Niepokojczycki <krzysztof.niepokojczycki@gmail.com>
2019-04-11 12:34:47 +0200

    add problem 2

---
+++ problem2/problem.go
@@ -0,0 +1,30 @@
+package problem2
+
+import "fmt"
+
+//By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.
+func evenFibonacciSum(limit int) int {
+	values := []int{1, 1}
+
+	for true {
+		newValue := values[len(values)-1] + values[len(values)-2]
+
+		if newValue >= limit {
+			break
+		}
+		values = append(values, newValue)
+	}
+
+	sum := 0
+
+	for _, val := range values {
+		if val%2 == 0 {
+			sum += val
+		}
+	}
+	return sum
+}
+
+func Result() {
+	fmt.Printf("Result for 1000 is %d\n", evenFibonacciSum(4*1000*1000))
+}
---
+++ problem2/problem_test.go
@@ -0,0 +1,34 @@
+package problem2
+
+import "testing"
+
+func Test_evenFibonacciSum(t *testing.T) {
+	type args struct {
+		limit int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			"test case",
+			args{
+				90,
+			},
+			44,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := evenFibonacciSum(tt.args.limit); got != tt.want {
+				t.Errorf("evenFibonacciSum() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+
+func Test_Result(t *testing.T) {
+	Result()
+}
