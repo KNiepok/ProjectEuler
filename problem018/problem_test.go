d721894aece0da6a02f4505107d535bdc03e0778
Krzysztof Niepokojczycki <krzysztof.niepokojczycki@gmail.com>
2019-04-12 11:18:32 +0200

    solve problem 18

---
+++ problem018/problem_test.go
@@ -0,0 +1,39 @@
+package problem018
+
+import "testing"
+
+func Test_maximumPath(t *testing.T) {
+	type args struct {
+		triangle [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			"test",
+			args{
+				triangle: [][]int{
+					{3},
+					{7,4},
+					{2,4,6},
+					{8,5,9,3},
+				},
+			},
+			23,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maximumPath(tt.args.triangle); got != tt.want {
+				t.Errorf("maximumPath() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_Result(t *testing.T) {
+	Result()
+}
+
